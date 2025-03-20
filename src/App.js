84eea0f7e3d5d95b1b72da3514f45eb8956e7c5f
AlexanderGurinovich <rabotoguru@gmail.com>
2025-03-20 09:54:13 +0300

    fir workflows

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
-import { render } from 'react-dom'
 import { createStore } from 'redux'
 import MainApp from './components/MainApp'
 import reducer from './redux/reducers'
@@ -11,20 +8,10 @@ const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window
 
 
 
-
 function App() {
   return (
     <Provider store={store}>
       <MainApp />
-      {/* <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<PostsMainPage />}></Route>
-          <Route path="/posts/:postId" element={<SinglePostPage />} />
-          <Route path="/editPost/:postId" element={<EditPostForm />} />
-        </Routes>
-      </div>
-    </Router> */}
     </Provider>
   );
 }
